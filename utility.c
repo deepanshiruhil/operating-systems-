23203d6a7e1952134a3c142d76ee65d5a53a5db4
deepanshiruhil <178875578+deepanshiruhil@users.noreply.github.com>
2026-05-09 09:31:15 +0530

    Implement error handling for key generation
    Added error handling for key generation failure.

--- utility.c
+++ utility.c
@@ -7,6 +7,7 @@ int get_shared_block(char *filename, int size)
 //request a key
 //the key is linked to a filename, so that other programs can access it.
 //rhe file MUST exist+ also be accessible.
+    
     key_t key = ftok(filename, 0);
     if (key == FAILURE) 
     {
